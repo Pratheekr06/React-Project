3aeb62a7ce82be04de908ec79471d5526cb30877
unknown <pratheekr06@gmail.com>
2020-05-23 17:52:29 +0530

    final commit after deploy

---
+++ build/precache-manifest.bfb485e39087211bc0c00b415e48bf13.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b539108511239d1a6696abed1158039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7812ec36f7112485edce",
+    "url": "/static/css/3.beefb2e5.chunk.css"
+  },
+  {
+    "revision": "25ad14e49ed5183190cc",
+    "url": "/static/css/4.fcd5cdec.chunk.css"
+  },
+  {
+    "revision": "547113d3b0a094824328",
+    "url": "/static/css/5.1ef06d83.chunk.css"
+  },
+  {
+    "revision": "732172303ffaba20578a",
+    "url": "/static/css/main.89036479.chunk.css"
+  },
+  {
+    "revision": "cc5978a42d4350652665",
+    "url": "/static/js/2.7fe8dfa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7fe8dfa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7812ec36f7112485edce",
+    "url": "/static/js/3.8aeca49e.chunk.js"
+  },
+  {
+    "revision": "25ad14e49ed5183190cc",
+    "url": "/static/js/4.f82e266f.chunk.js"
+  },
+  {
+    "revision": "547113d3b0a094824328",
+    "url": "/static/js/5.0bd9d318.chunk.js"
+  },
+  {
+    "revision": "732172303ffaba20578a",
+    "url": "/static/js/main.bf607298.chunk.js"
+  },
+  {
+    "revision": "3ee66c187123da46353e",
+    "url": "/static/js/runtime-main.9217a2b9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -50,7 +50,7 @@ class Auth extends Component {
   }
 
   inputChangedHandler = (event, controlName) => {
-    const updatedControls = updateObject(this.st4.controls, {
+    const updatedControls = updateObject(this.state.controls, {
       [controlName]: updateObject(this.state.controls[controlName], {
         value: event.target.value,
         valid: checkValidity(
